bb55b1de683789e2c6374f99e5a3dc1aa3f30793
Natasha <natkamonte1992@gmail.com>
2022-11-07 22:32:24 +0200

    ,

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+import throttle from 'lodash.throttle';
 
 const email = document.querySelector("input");
 const message = document.querySelector('textarea');
@@ -5,16 +6,19 @@ const submitButton = document.querySelector('button');
 let userData = {};
 
 
-email.addEventListener('input', _.throttle(onInputChange, 500ms));
+email.addEventListener('input', throttle(onInputChange, 500));
 function onInputChange(event) {
-    localStorage.setItem('feedback-form-state-email', event.target.value);
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(userData)
+    );
     userData.email = event.target.value;
-    console.log(userData)
+  console.log(userData)
 };
 
-message.addEventListener('input', _.throttle(onMessageEnter, 500ms));
+message.addEventListener('input', throttle(onMessageEnter, 500));
 function onMessageEnter(event) {
-    localStorage.setItem('feedback-form-state-message', event.target.value);
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
     userData.message = event.target.value;
     console.log(userData);
 };
@@ -22,5 +26,14 @@ function onMessageEnter(event) {
 submitButton.addEventListener('click', onButtonClick);
 function onButtonClick(event) {
   localStorage.clear();
+};
+
+function onPageReboot(event) {
+  if (email.value.length === 0) {
+    email.value = JSON.parse(localStorage.getItem('feedback-form-state'));
+  }
+  if (message.value === '') {
+    message.value = JSON.parse(localStorage.getItem('feedback-form-state'));
+  }
 };
 
