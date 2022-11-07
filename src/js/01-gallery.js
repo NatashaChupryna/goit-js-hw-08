cd01eaddf585baf7a715c30daed4bb70248defca
Natasha <natkamonte1992@gmail.com>
2022-11-07 13:50:42 +0200

    exercise 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+`<li>
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+  )
+  .join('');
+
+const imageList = document.querySelector('.gallery');
+imageList.style.listStyle = 'none'
+imageList.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.5,
+});
 
 console.log(galleryItems);
